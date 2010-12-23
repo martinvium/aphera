ccae335570c478c382b03d791a238987b790f26d
Martin Vium <martin.vium@gmail.com>
2010-12-23 19:07:52 +0100

    Fix newlines

--- library/Aphera/Core/ApheraConfiguration.php
+++ library/Aphera/Core/ApheraConfiguration.php
@@ -5,40 +5,37 @@ class ApheraConfiguration implements Configuration
 {
     protected $extensionFactories = array();
     
-    
-    public static function getDefault()
-    {
+    public static function getDefault() {
         return new ApheraConfiguration();
     }
 
-    public function addExtensionFactory(ExtensionFactory $factory)
-    {
+    public function addExtensionFactory(ExtensionFactory $factory) {
         $this->extensionFactories[] = $factory;
     }
 
-    public function getConfigurationOption($id, $default = null)
-    {
-        $value = \getenv($id);
+    public function getConfigurationOption($id, $default = null) {
+        $value = $this->getEnvironmentVariable($id);
         if($default !== null) {
             $value = ($value ? $value : $default);
         }
         
         return $value;
     }
+    
+    protected function getEnvironmentVariable($id) {
+        return \getenv($id);
+    }
 
-    public function getExtensionFactories()
-    {
+    public function getExtensionFactories() {
         return $this->extensionFactories;
     }
 
-    public function newFactoryInstance(Aphera $aphera)
-    {
+    public function newFactoryInstance(Aphera $aphera) {
         $class = $this->getConfigurationOption(Constants::CONFIG_FACTORY, Constants::DEFAULT_FACTORY);
         return new $class($aphera);
     }
 
-    public function newParserInstance(Aphera $aphera)
-    {
+    public function newParserInstance(Aphera $aphera) {
         $class = $this->getConfigurationOption(Constants::CONFIG_PARSER, Constants::DEFAULT_PARSER);
         return new $class($aphera);
     }

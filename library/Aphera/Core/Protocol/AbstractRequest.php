225cf75af5defc18f7fde2b1bb7fd5a0b58d55eb
Martin Vium <martin.vium@gmail.com>
2010-12-24 13:02:02 +0100

    Fix AbstractRequest using the wrong interface (response)

--- library/Aphera/Core/Protocol/AbstractRequest.php
+++ library/Aphera/Core/Protocol/AbstractRequest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Aphera\Core\Protocol;
 
-abstract class AbstractRequest extends AbstractMessage implements Response
+abstract class AbstractRequest extends AbstractMessage implements Request
 {
     public function getAccept() {
         return $this->getHeader('Accept');

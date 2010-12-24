1d97bfe2a77f415dd3300b42f2570a96149c88a3
Martin Vium <martin.vium@gmail.com>
2010-12-24 13:03:13 +0100

    Forgot to commit RequestContext interface update

--- library/Aphera/Server/RequestContext.php
+++ library/Aphera/Server/RequestContext.php
@@ -2,8 +2,26 @@
 namespace Aphera\Server;
 
 use Aphera\Core\Protocol\Request;
+use Aphera\Core\Parser;
 
 interface RequestContext extends Request
 {
+    /**
+     * @return Aphera\Core\Aphera
+     */
+    public function getAphera();
     
+    public function getMethod();
+    
+    public function getUri();
+    
+    public function getResolvedUri();
+    
+    public function getBaseUri();
+    
+    public function getDocument(Parser $parser);
+    
+    public function getInputStream();
+    
+    public function getTargetType();
 }

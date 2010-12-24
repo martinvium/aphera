c996b6a9e2e9ce5d860038a6283949d61123d994
Martin Vium <martin.vium@gmail.com>
2010-12-24 12:58:27 +0100

    Add missing methods to parser interface

--- library/Aphera/Core/Parser.php
+++ library/Aphera/Core/Parser.php
@@ -1,7 +1,19 @@
 <?php
 namespace Aphera\Core;
 
+use Aphera\Model\Document;
+
 interface Parser
 {
+    /**
+     * @param resource $handle 
+     * @return Document
+     */
+    public function parseStream($handle);
     
+    /**
+     * @param string $xml
+     * @return Document
+     */
+    public function parseString($xml);
 }

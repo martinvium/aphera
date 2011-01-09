174cbb2ebacbca19df05d3e661634dac61e3a6cb
Martin Vium <martin.vium@gmail.com>
2011-01-09 21:13:06 +0100

    Add notFound method shorthand for 404's

--- library/Aphera/Server/ProviderHelper.php
+++ library/Aphera/Server/ProviderHelper.php
@@ -1,25 +1,26 @@
 <?php
 namespace Aphera\Server;
 
-use Aphera\Server\Context;
+use Aphera\Server\Context\EmptyResponseContext;
 
 class ProviderHelper
 {
-    public static function notAllowed(RequestContext $request, $message)
-    {
+    public static function notAllowed(RequestContext $request, $message) {
         return self::createErrorResponse(405, $message);
     }
     
-    public static function notAllowedMethod(RequestContext $request)
-    {
+    public static function notAllowedMethod(RequestContext $request) {
         return self::notAllowed($request, "Method Not Allowed");
     }
     
-    public static function createErrorResponse($code, $message)
-    {
-        $rc = new Context\SimpleResponseContext();
+    public static function notFound(RequestContext $request) {
+        return self::createErrorResponse(404, "Not Found");
+    }
+    
+    public static function createErrorResponse($code, $message) {
+        $rc = new EmptyResponseContext();
         $rc->setStatus($code);
-        $rc->setMessage($message);
+        $rc->setStatusText($message);
         return $rc;
     }
 }
